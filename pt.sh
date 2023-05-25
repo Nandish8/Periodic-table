c3007d47bab863e5d781163656c632e03dfaeb9e
Nandish8 <132594041+Nandish8@users.noreply.github.com>
2023-05-26 02:27:51 +0530

    Update pt.sh

--- pt.sh
+++ pt.sh
@@ -62,3 +62,4 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 # echo $U_TRAILING_ZEROS_ATM
 # INSERT_FE=$($PSQL "INSERT INTO elements(atomic_number, symbol, name) VALUES(9, 'F', 'Fluorine'), (10, 'Ne', 'Neon')")
 # INSERT_F=$($PSQL "INSERT INTO properties(atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id) VALUES(9, 'nonmetal', 18.998, -220, -188.1, 1), (10, 'nonmetal', 20.18, -248.6, -246.1, 1)")
+# DELETE_TYPE=$($PSQL "ALTER TABLE properties DROP COLUMN type")
